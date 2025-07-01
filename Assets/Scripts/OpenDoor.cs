5b53a279a4a48d7f4b7c20c9f98ad5eeb7ee8d6d
CochiwonSang <tkdals1596@gmail.com>
2025-07-01 17:40:06 +0900

    Miyaong

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 한상민이 만든 스크립트, 문 오픈 스크립트
+/// </summary>
+public class OpenDoor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
